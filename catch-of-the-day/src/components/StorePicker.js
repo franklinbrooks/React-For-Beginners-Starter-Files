6e4b96bb7fa918f9e713993a6e4b3457f2265555
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2016-12-30 14:26:30 -0500

    R4B chapter 10

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
@@ -7,7 +8,7 @@ class StorePicker extends React.Component {
       <form className="store-selector">
         {/* Inside JSX, comments must go in curly braces with CSS open/close */ }
         <h2>Please select a store</h2>
-        <input type='text' required placeholder='Store Name'/>
+        <input type='text' required placeholder='Store Name' defaultValue={getFunName()}/>
         <button type='submit'>Visit Store</button>
       </form>
       )
