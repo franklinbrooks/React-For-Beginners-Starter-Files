5077bf092990af7e642e2860d9864b33470b08ec
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2016-12-31 18:49:05 -0500

    R4B chapter 18

---
+++ catch-of-the-day/src/Base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+ // pass to base 1) api key, 2) domain conectng to, and 3) database url
+const base = Rebase.createClass({
+  apiKey: "AIzaSyB-RtQwE4fbBR6hmO_vD3JOV63uqekc2W0",
+  authDomain: "catch-of-the-day-f-c-brooks.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-f-c-brooks.firebaseio.com",
+});
+
+export default base;
+
