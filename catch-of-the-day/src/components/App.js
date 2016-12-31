d0548890de80bdf190deab6682f816deefb72aab
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2016-12-31 00:20:54 -0500

    R4B chapter 14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.addFish = this.addFish.bind(this);
+    this.addFish = this.addFish.bind(this); // allows access to 'this' context App inside functions below
+    this.loadSamples = this.loadSamples.bind(this);
     // getInitialState
     this.state = {
       fishes: {},
@@ -23,6 +25,12 @@ class App extends React.Component {
     this.setState({ fishes: fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -30,7 +38,7 @@ class App extends React.Component {
           <Header tagline='Fresh Seafood Market'/>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
       )
   }
