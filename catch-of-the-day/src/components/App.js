c093cfe0392979e8ef9bf89242d65b797c5c2c32
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2016-12-31 23:28:40 -0500

    R4B chapter 19

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -20,15 +20,30 @@ class App extends React.Component {
   }
 
   componentWillMount() {
+    // runs right before App is rendered
     this.ref = base.syncState(`${this.props.params.storeId}/fishes`
       , {
       context: this,
       state: 'fishes'
       });
+    // check if there is an order in localStorage
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+
+    if(localStorageRef) {
+      // then update App component localStorage state
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`,
+      JSON.stringify(nextState.order));
   }
 
   addFish(fish) {
@@ -68,7 +83,11 @@ class App extends React.Component {
               }
             </ul>
         </div>
-        <Order fishes={this.state.fishes} order={this.state.order} />
+        <Order
+          fishes={this.state.fishes}
+          order={this.state.order}
+          params = {this.props.params}
+        />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
       </div>
       )
